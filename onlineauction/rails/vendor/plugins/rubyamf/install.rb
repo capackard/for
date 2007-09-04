06404e1f55b602ea43489d66cb3f8a989d97c306
danielwanja <danielwanja@43b9b960-c338-0410-b68b-897081851b70>
2007-09-04 02:59:54 +0000

    Piston import of rubyamf (trunk) - 1.3rc2

---
+++ onlineauction/rails/vendor/plugins/rubyamf/install.rb
@@ -0,0 +1,36 @@
+#This Install script is for Rails Plugin Installation. If using the RubyAMF Lite this is not needed.
+begin
+  require 'fileutils'
+  
+  if !File.exist?('./config/rubyamf/')
+    Dir.mkdir("./config/rubyamf/")
+  end
+  
+  if !File.exist?('./config/rubyamf/vo_config.rb')
+    FileUtils.copy_file("./vendor/plugins/rubyamf/rubyamf_core/rails_installer_files/vo_config.rb", "./config/rubyamf/vo_config.rb", false)
+  end
+  
+  if !File.exist?('./config/rubyamf/adapters_config.rb')
+    FileUtils.copy_file("./vendor/plugins/rubyamf/rubyamf_core/rails_installer_files/adapters_config.rb", "./config/rubyamf/adapters_config.rb", false)
+  end
+  
+  FileUtils.copy_file("./vendor/plugins/rubyamf/rubyamf_core/rails_installer_files/rubyamf_controller.rb","./app/controllers/rubyamf_controller.rb",false)
+  FileUtils.copy_file("./vendor/plugins/rubyamf/rubyamf_core/rails_installer_files/crossdomain.xml","./public/crossdomain.xml", false)
+  
+  mime = true
+  File.open("./config/environment.rb","r") do |f|
+    while line = f.gets
+      if line.match(/application\/x-amf/)
+        mime = false
+      end
+    end
+  end
+  
+  if mime
+    File.open("./config/environment.rb","a") do |f|
+      f.puts "\nMime::Type.register \"application/x-amf\", :amf"
+    end
+  end
+rescue Exception => e
+  puts "ERROR INSTALLING RUBYAMF: " + e.message
+end
---
+++ onlineauction/rails/vendor/plugins/rubyamf/rubyamf_core/app/configuration.rb
@@ -0,0 +1,121 @@
+#Copyright (c) 2007 Aaron Smith (aaron@rubyamf.org) - MIT License
+
+#This stores supporting configuration classes used in the config file to register, adapters, vo's, application instances, etc.
+
+module RUBYAMF
+module Configuration
+
+#Application::Instance support class
+class Application
+  
+  module Instance
+    @@app_instances = []
+    
+    #register's an application instance
+    def Instance.register(definition)
+      if definition[:source] == nil || definition[:source] == ''
+        return nil
+      end
+      if definition[:name] == nil || definition[:name] == ''
+        return nil
+      end
+      if @@app_instances == nil
+        @@app_instances = []
+      end
+      @@app_instances << definition
+    end
+    
+    #Get an application instance definition from a target service
+    def Instance.getAppInstanceDefFromTargetService(source)
+      if source.nil?
+        return nil
+      end
+      
+      final_ai = nil
+      @@app_instances.each do |ai|
+        m = ai[:source].clone
+        #create a simple regex here from a source package
+        #org.rubyamf.amf.* => org\.rubyamf\.amf\..*
+        #used to match against the ai[:source]
+        m.gsub!('.','\.')
+        m.gsub!('*','.*')
+        if source.match(m)
+          return ai
+        end        
+      end
+      nil
+    end
+  end
+end
+
+#Global adapters configuration support class
+class Adapters
+  @@adpters = []
+  def Adapters.register(file,classname)
+    @@adpters << [file,classname]
+  end
+  
+  def Adapters.get_adapters
+    return @@adpters
+  end
+end
+
+#ValueObjects configuration support class
+class ValueObjects
+  
+  @@vo_mappings = []
+  @@vo_by_instances_lookup = {}
+  @@mapping_type = ''
+
+  #register a value object map
+  def ValueObjects.register(hash)
+    if hash[:instance] != nil
+      if @@vo_by_instances_lookup[hash[:instance]].nil?
+        @@vo_by_instances_lookup[hash[:instance]] = []
+      end
+      @@vo_by_instances_lookup[hash[:instance]] << hash
+    else
+      @@vo_mappings << hash
+    end
+  end
+    
+  #Get ValueObject mappings for this request
+  def ValueObjects.get_vo_mappings
+    maps = []
+    #If no application instance has been defined for this request, just return all ValueObjects
+    if RequestStore.app_instance == nil
+      if !@@vo_mappings.empty? #if some global value objects are available, put them in the maps array
+        maps.concat(@@vo_mappings)
+      end
+      return maps
+    else
+      instance = RequestStore.app_instance[:name]
+      #puts instance specific VO's into the maps array
+      if !@@vo_by_instances_lookup.empty?
+        if @@vo_by_instances_lookup[instance] != nil
+          maps.concat(@@vo_by_instances_lookup[instance])
+        end
+      end
+      
+      #now put non app specific ValueObjects into maps array
+      if !@@vo_mappings.empty?
+        maps.concat(@@vo_mappings)
+      end
+    end
+    maps
+  end
+  
+  #the rails parameter mapping type
+  def ValueObjects.rails_parameter_mapping_type=(val)
+    @@mapping_type = val
+  end
+  
+  #the rails parameter mapping type
+  def ValueObjects.rails_parameter_mapping_type
+    @@mapping_type
+  end
+  
+end
+
+end
+end
