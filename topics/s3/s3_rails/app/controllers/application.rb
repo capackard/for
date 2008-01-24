1d6854e95ffa8aeb4f465523bb31d6ca969c9159
danielwanja <danielwanja@43b9b960-c338-0410-b68b-897081851b70>
2008-01-24 23:42:48 +0000

    Initial checkin of s3 asset manager

---
+++ topics/s3/s3_rails/app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '960deeb1732bc1fee5081d80b2d9485a'
+end
