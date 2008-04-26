df36164e773b0e3686f0d319b3e08b3f8b1c15ba
tony.hillerson <tony.hillerson@43b9b960-c338-0410-b68b-897081851b70>
2008-04-26 21:03:11 +0000

    more work on the rails standard rubyamf example

--- trunk/topics/rubyamf/rails_standard/app/controllers/contacts_controller.rb
+++ trunk/topics/rubyamf/rails_standard/app/controllers/contacts_controller.rb
@@ -1,34 +1,47 @@
 class ContactsController < ApplicationController
-
+  
   # return all Contacts
   def find_all
-    render :amf => Contact.find(:all)
+    respond_to do |format|
+      format.amf  { render :amf => Contact.find(:all) }
+    end
   end
   
-  # return a single Contact, by id
-  # expect single argument: contact id
+  # return a single Contact by id
+  # expects id in params[0]
   def find_by_id
-    render :amf => Contact.find(params[:id])
+    respond_to do |format|
+      format.amf { render :amf => Contact.find(params[0]) }
+    end
   end
 
-  # saves new or updates existing Contacts
+  # saves new or updates existing Contact
   # expect params[0] to be incoming Contact
   def save
-    @contact = params[0]
+    respond_to do |format|
+      format.amf do
+        @contact = params[0]
 
-    if @contact.save
-      render :amf => @contact
-    else
-      render :amf => FaultObject.new(@contact.errors.full_messages.join('\n'))
+        if @contact.save
+          render :amf => @contact
+        else
+          render :amf => FaultObject.new(@contact.errors.full_messages.join('\n'))
+        end
+      end
     end
   end
 
   # destroy a Contact
-  # expect single argument: product id
+  # expects id in params[0]
   def destroy
-    @contact = Contact.find(params[:id])
-    @contact.destroy
+    respond_to do |format|
+      format.amf do
+        @contact = Contact.find(params[0])
+        @contact.destroy
 
-    render :amf => true
+        render :amf => true
+      end
+    end
   end
+
 end
