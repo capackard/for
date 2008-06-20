4be395f068618948137fce3eb73b37c7ac85391b
tony.hillerson <tony.hillerson@43b9b960-c338-0410-b68b-897081851b70>
2008-06-20 06:24:29 +0000

    to prana sequence cookbook example code: added cairngen, wired up logging in and getting notes with prana sequence. need view work

---
+++ topics/cookbook/prana_sequences/flex/src/com/flexonrails/pranaSequences/events/LoadUserNotesEvent.as
@@ -0,0 +1,17 @@
+package com.flexonrails.pranaSequences.events {
+	
+	import com.adobe.cairngorm.control.CairngormEvent;
+	import com.flexonrails.pranaSequences.vo.User;
+
+	public class LoadUserNotesEvent extends CairngormEvent {
+		
+		public static const LoadUserNotes_Event:String = "<LoadUserNotesEvent>";
+		
+		public var user:User;
+		
+		public function LoadUserNotesEvent(user:User) {
+			super(LoadUserNotes_Event);
+			this.user = user;
+		}
+	}
+}
--- topics/cookbook/prana_sequences/rails/config/rubyamf_config.rb
+++ topics/cookbook/prana_sequences/rails/config/rubyamf_config.rb
@@ -2,6 +2,7 @@ require 'app/configuration'
 module RubyAMF
   module Configuration
     ClassMappings.translate_case = true
+    ClassMappings.use_array_collection = true
     ParameterMappings.scaffolding = true
   
     ClassMappings.register(
@@ -9,5 +10,12 @@ module RubyAMF
       :ruby          => 'User',
       :type          => 'active_record',
       :attributes    => ["id", "login", "email", "created_at", "updated_at"])
+      
+    ClassMappings.register(
+      :actionscript  => 'Note',
+      :ruby          => 'Note',
+      :type          => 'active_record',
+      :attributes    => ["id", "content", "title", "user_id", "created_at", "updated_at"])
+    
   end
 end
