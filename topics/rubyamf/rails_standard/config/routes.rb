061ce4467b7dc93880d1e72b45891899925336e2
tony.hillerson <tony.hillerson@43b9b960-c338-0410-b68b-897081851b70>
2008-10-16 02:03:38 +0000

    moved rubyamf to github

--- topics/rubyamf/rails_standard/config/routes.rb
+++
@@ -1,7 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.rubyamf_gateway 'rubyamf_gateway', :controller => 'rubyamf', :action => 'gateway'
-
-  map.connect 'rubyamf_gateway', :controller => 'rubyamf', :action => 'gateway'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
-end
